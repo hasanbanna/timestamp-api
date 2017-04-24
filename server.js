dbd878184b0b739fcb01edd8a1e15484847a17dc
HasanBanna <hbann002@gold.ac.uk>
2017-04-24 12:22:25 +0000

    unix time conversion testing

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname+'/index.html'))
+})
+// convert unix time to a date
+app.get('/date', function(req,res){
+  var date = new Date(req.query.unix_time*1000);
+  res.send(date);
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
